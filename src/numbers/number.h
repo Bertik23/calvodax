d912a0582f0797dc0f0708a37340032001035c40
Albert Havlicek <havlialb@fit.cvut.cz>
2023-05-04 20:30:41 +0200

    number.h: number definition

--- src/numbers/number.h
+++ src/numbers/number.h
@@ -1,25 +1,38 @@
+#include <vector>
+#include <iostream>
+#include "../typedefs.h"
+
 class Number{
 public:
-    Number(long long);
+    Number();
+    Number(i32);
     Number(const Number &);
     Number(Number &&);
     virtual Number & operator = (const Number &);
     virtual Number & operator = (Number &&);
-    virtual Number operator + (const Number &);
-    virtual Number operator - (const Number &);
-    virtual Number operator * (const Number &);
-    virtual Number operator / (const Number &);
-    virtual Number operator ^ (const Number &);
-    virtual Number operator | (const Number &);
-    virtual Number operator & (const Number &);
-    virtual bool operator < (const Number &);
-    virtual bool operator > (const Number &);
-    virtual bool operator == (const Number &);
+    virtual Number operator + (const Number &) const;
+    //virtual Number operator - (const Number &) const;
+    virtual Number operator * (const Number &) const;
+    //virtual Number operator / (const Number &) const;
+    //virtual Number operator ^ (const Number &) const;
+    //virtual Number operator | (const Number &) const;
+    //virtual Number operator & (const Number &) const;
+    virtual bool operator < (const Number &) const;
+    //virtual bool operator > (const Number &) const;
+    //virtual bool operator == (const Number &) const;
     virtual Number & operator += (const Number &);
-    virtual Number & operator -= (const Number &);
+    //virtual Number & operator -= (const Number &);
     virtual Number & operator *= (const Number &);
-    virtual Number & operator /= (const Number &);
-    virtual Number & operator ^= (const Number &);
-    virtual Number & operator |= (const Number &);
-    virtual Number & operator &= (const Number &);
+    //virtual Number & operator /= (const Number &);
+    //virtual Number & operator ^= (const Number &);
+    //virtual Number & operator |= (const Number &);
+    //virtual Number & operator &= (const Number &);
+    virtual void print(std::ostream &) const;
+    friend std::ostream & operator << (std::ostream & os, const Number & n){
+        n.print(os);
+        return os;
+    }
+private:
+    std::vector<u32> numbers;
+    bool is_negative = false;
 };
