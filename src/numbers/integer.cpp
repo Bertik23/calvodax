f521d65ea5eebb5443afc27daf454389f4f987ba
Albert Havlicek <havlialb@fit.cvut.cz>
2023-05-04 20:31:06 +0200

    integer.cpp: number code

---
+++ src/numbers/integer.cpp
@@ -0,0 +1,64 @@
+#include "number.h"
+#include <cmath>
+
+
+Number::Number() = default;
+
+Number::Number(i32 i)
+    : numbers{(u32) std::abs(i)},
+      is_negative(i < 0){}
+
+Number::Number(const Number & o) = default;
+Number::Number(Number && o) = default;
+Number & Number::operator = (const Number &) = default;
+Number & Number::operator = (Number &&) = default;
+
+Number & Number::operator += (const Number & other){
+    std::size_t i = 0;
+    unsigned long long cary = 0;
+    for (;
+         i < std::min(numbers.size(), other.numbers.size());
+        ++i
+    ){
+        auto prev = numbers[i];
+        numbers[i] += other.numbers[i] + cary;
+        if (numbers[i] < prev){
+            cary = 1;
+        } else cary = 0;
+    }
+    while (cary){
+        if (i < numbers.size()){
+            // i >= other.numbers.size()
+            auto prev = numbers[i];
+            numbers[i] += cary;
+            if (numbers[i] < prev){
+                cary = 1;
+            } else cary = 0;
+        } else if (i >= numbers.size() && i < other.numbers.size()) {
+            numbers.push_back(other.numbers[i] + cary);
+            if (numbers[i] < other.numbers[i]){
+                cary = 1;
+            } else cary = 0;
+        } else if (i >= numbers.size() && i >= other.numbers.size()){
+            numbers.push_back(cary);
+            cary = 0;
+        }
+        ++i;
+    }
+    while(i < other.numbers.size()){
+        numbers.push_back(other.numbers[i++]);
+    }
+    return *this;
+}
+
+Number Number::operator + (const Number & other) const{
+    Number tmp(*this);
+    return tmp += other;
+}
+
+void Number::print(std::ostream & os) const {
+    for (auto & i: numbers){
+        os << i;
+        os << ", ";
+    }
+}
