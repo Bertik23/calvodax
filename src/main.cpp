ff4b567e9bd4486efb28fc8d8037c554f3e5b51b
Albert Havlicek <havlialb@fit.cvut.cz>
2023-05-05 15:06:01 +0200

    main: more tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "numbers/number.h"
+#include <cassert>
 
 #include <iostream>
 
@@ -12,5 +13,23 @@ int main(){
 
     cout << a + a + a + a + a + a + a + a << endl;
 
+    cout << "2^31 - 1 × 2^31 - 1" << endl;
     cout << a * a << endl;
+
+    Number c(0);
+    Number d(-1);
+
+    cout << "0 × 0" << endl;
+    cout << ">>> " << c * c << endl;
+    cout << "-1 × -1" << endl;
+    cout << ">>> " << d * d << endl;
+    cout << "0 × 20" << endl;
+    cout << ">>> " << c * b << endl;
+    cout << "0 × -1" << endl;
+    cout << ">>> " << c * d << endl;
+
+    assert(Number(1) < Number(2));
+    assert(Number(0) < Number(1));
+    assert(!(Number(1) < Number(0)));
+    assert(Number(-1) < Number(1));
 }
