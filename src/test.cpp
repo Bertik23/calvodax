c7cd09f223620ebfdb6f82a7629a827ebcfb96f2
Albert Havlicek <havlialb@fit.cvut.cz>
2023-05-23 18:06:50 +0200

    test: test power

--- src/test.cpp
+++ src/test.cpp
@@ -7,4 +7,6 @@ using namespace std;
 int main(){
     cout << Rational(2000, 2) << endl;
     cout << (Integer(1) == Integer(1)) << endl;
+    cout << Integer(5).power(Integer(2)) << endl;
+    cout << Rational(5).power(Rational(2)) << endl;
 }
