f493874039d1a223d002b46434daf083a2bd4f2c
Albert Havlicek <havlialb@fit.cvut.cz>
2023-05-23 14:14:21 +0200

    number: number representation

--- src/numbers/numberrepresentation.h
+++ src/numbers/numberrepresentation.h
@@ -2,12 +2,14 @@
 
 #include "digits.h"
 #include <memory>
+#include <iostream>
 
 
 class NumberRepresentation{
 public:
     virtual ~NumberRepresentation() = default;
     virtual Digits get_digits() const = 0;
+    virtual void print(std::ostream &) const = 0;
 };
 
 std::shared_ptr<NumberRepresentation> find_representation(const Digits &);
